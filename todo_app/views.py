5fd181e404769e2c66b83a28a28b7aecbd74a0ca
Erfan <erfoonmaraghi@gmail.com>
2024-06-27 10:52:56 +0330

    TodosList View Wrote.

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Todo
+from .serializer import TodoSerializer
 
-# Create your views here.
+
+class TodosView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
