92f16152ced6f07ec1e5076b0dd35c4533ce6bba
Erfan <erfoonmaraghi@gmail.com>
2024-06-27 10:41:31 +0330

    Todo model created & migrated.

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Title")
+    description = models.TextField(verbose_name="Description")
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Todo"
+        verbose_name_plural = "Todos"
+        db_table = "todos"
+        ordering = ["-updated_at", "-created_at"]
+
