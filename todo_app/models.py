52b38bfe2d36c9444d06e11dc86670a01557e7bf
Erfan <erfoonmaraghi@gmail.com>
2024-06-27 10:43:51 +0330

    description field of Todo model got nullable.

--- todo_app/models.py
+++ todo_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=100, verbose_name="Title")
-    description = models.TextField(verbose_name="Description")
+    description = models.TextField(verbose_name="Description", null=True, blank=True)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -16,4 +16,3 @@ class Todo(models.Model):
         verbose_name_plural = "Todos"
         db_table = "todos"
         ordering = ["-updated_at", "-created_at"]
-
